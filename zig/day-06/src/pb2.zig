3f2ec9f50fdba61eee165c4e359a1a048c3789e1
SamuelDarras <slamicote@gmail.com>
2025-12-06 17:32:09 +0100

    this version can account for any number of lines, but is slower

--- zig/day-06/src/pb2.zig
+++ zig/day-06/src/pb2.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 
 const SRC = @embedFile("./src1.txt");
 // const SRC =
-//     \\123 328  51 64 
-//     \\ 45 64  387 23 
+//     \\123 328  51 64
+//     \\ 45 64  387 23
 //     \\  6 98  215 314
 //     \\*   +   *   +
 //     \\
@@ -11,18 +11,22 @@ const SRC = @embedFile("./src1.txt");
 const LINE_COUNT: usize = 4;
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const alloc = gpa.allocator();
+
     const line_length = std.mem.indexOf(u8, SRC, "\n").? + 1;
+    const line_count = std.mem.count(u8, SRC, "\n") - 1;
 
     var col_symbol: u8 = ' ';
     var sum: usize = 0;
     var column_sum: usize = 0;
     var column_product: usize = 1;
     for (0..line_length - 1) |idx| {
-        if (SRC[idx + line_length * LINE_COUNT] == '+' or SRC[idx + line_length * LINE_COUNT] == '*') col_symbol = SRC[idx + line_length * LINE_COUNT];
+        if (SRC[idx + line_length * line_count] == '+' or SRC[idx + line_length * line_count] == '*') col_symbol = SRC[idx + line_length * line_count];
 
         const is_break = blk: {
             var is_break = true;
-            for (0..LINE_COUNT + 1) |l| {
+            for (0..line_count + 1) |l| {
                 is_break &= (SRC[idx + line_length * l] == ' ');
             }
             break :blk is_break;
@@ -36,7 +40,7 @@ pub fn main() !void {
             column_sum = 0;
             column_product = 1;
         } else {
-            const v = try value_from_col(idx, line_length);
+            const v = try value_from_col(alloc, idx, line_length, line_count);
             column_sum += v;
             column_product *= v;
         }
@@ -53,12 +57,13 @@ pub fn main() !void {
     std.debug.print("{d}\n", .{sum});
 }
 
-fn value_from_col(idx: usize, len: usize) !usize {
-    var buffer: [LINE_COUNT]u8 = undefined;
+fn value_from_col(alloc: std.mem.Allocator, idx: usize, len: usize, line_count: usize) !usize {
+    var buffer = try std.ArrayList(u8).initCapacity(alloc, 16);
+    defer buffer.deinit(alloc);
 
-    for (0..LINE_COUNT) |m| {
-        buffer[m] = SRC[idx + m * len];
+    for (0..line_count) |m| {
+        try buffer.append(alloc, SRC[idx + m * len]);
     }
 
-    return try std.fmt.parseInt(usize, std.mem.trim(u8, &buffer, " "), 10);
+    return try std.fmt.parseInt(usize, std.mem.trim(u8, buffer.items, " "), 10);
 }
